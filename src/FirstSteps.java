690a13fbf58da9fd08b6dd919bbff080d6a94abd
HakuSenteng <willianhero3@gmail.com>
2023-02-27 16:09:33 -0300

    firsts steps
    - Declaration of all primitive variables

---
+++ src/FirstSteps.java
@@ -0,0 +1,27 @@
+public class FirstSteps {
+    byte small = 10;
+    /*Variables in "byte" have a range of -128 to 127. 
+     *Only goes for integer numbers.
+     */
+    short medium = 10000;
+    /*Variables in "short" have a range of -32768 to 32767 
+     * and also only goes for integer numbers.
+     */
+    int big = 1000000000;
+    /*Variables in "int" have a range of -2 billions to 2 billions.
+     * Only integer numbers. 
+     */
+    long bigger = 10000000000000L;
+    /*Variables in "long" have a range of -9 quadrillions to 9 quadrillions
+     * Only integer numbers.
+     */
+    float evenBigger = 10000000000000.5f;
+    /*Variables in "float" have a range of 1.4 E-45 to more than 3.4 E+38
+     * In "float" we can use floating-point numbers like 1,5
+      */
+    double biggest = 1.1;
+    /*Variables in "double" have the biggest range and also accepts floating points numbers*/    
+    boolean trueAndFalse = true;
+    /*Variables in boolean are only true and false */
+
+}
